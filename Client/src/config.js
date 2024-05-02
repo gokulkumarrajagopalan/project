23e070cf5326c5e14d7787ba3091c30c982ebc3b
gokulkumarrajagopalan <98198356+gokulkumarrajagopalan@users.noreply.github.com>
2024-05-02 15:33:30 +0530

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -3,9 +3,9 @@ const API_URLS = {
    // development: "http://localhost:3700",
    // production : "http://localhost:3700"
     
-    // production: "https://project-seven-plum.vercel.app"
+    production: "https://project-seven-plum.vercel.app"
 
-    production: "http://13.232.224.236:3700"
+    //production: "http://13.232.224.236:3700"
   };
   
   export default API_URLS;
