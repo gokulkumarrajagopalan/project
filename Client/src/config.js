89da3f83506c9f761e1060c17f7bc8ff998234a5
Gokul <gokulkumarrajagopalan@gmail.com>
2024-05-08 09:56:25 +0530

    commit

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,9 +1,9 @@
 
 const API_URLS = {
-   // development: "http://localhost:3700",
-   // production : "http://localhost:3700"
+   development: "http://localhost:3700",
+   production : "http://localhost:3700"
     
-    production: "https://project-seven-plum.vercel.app"
+    //production: "https://project-seven-plum.vercel.app"
   };
   
   export default API_URLS;
