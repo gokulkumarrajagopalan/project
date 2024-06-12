ba2fe2eda1b246b83537c6aedc9929901b935617
gokulkumarrajagopalan <98198356+gokulkumarrajagopalan@users.noreply.github.com>
2024-06-12 19:49:34 +0530

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -3,7 +3,7 @@ const API_URLS = {
    // development: "http://localhost:3700",
    // production : "http://localhost:3700"
     
-    production: "https://project-seven-plum.vercel.app"
+    production: "https://server.gdest.in"
 
     //production: "http://13.232.224.236:3700"
   };
