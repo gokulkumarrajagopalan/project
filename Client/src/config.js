f40c681b4e33f26c693080133f658b9783e06287
gokulkumarrajagopalan <98198356+gokulkumarrajagopalan@users.noreply.github.com>
2024-06-19 00:12:02 +0530

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -2,7 +2,7 @@ const API_URLS = {
   // development: "http://localhost:3700",
   // production : "http://localhost:3700"
 
-  // production: "https://server.gdest.in"
+   production: "https://server.gdest.in"
 
  // production: "https://lw7j96-3800.csb.app",
 };
