14273fb6b4fe408e299669e3aae1b1b32c337354
Gokul <98198356+gokulkumarrajagopalan@users.noreply.github.com>
2024-06-27 09:30:06 +0000

    commit

--- Client/src/config.js
+++ Client/src/config.js
@@ -2,11 +2,9 @@ const API_URLS = {
   // development: "http://localhost:3700",
   // production : "http://localhost:3700"
 
-  // production: "https://server.gdest.in"
+  production: "https://server.gdest.in",
 
- // production: "https://lw7j96-3800.csb.app",
+  // production: "https://lw7j96-3800.csb.app",
 };
 
 export default API_URLS;
-
-
