6e764c74e574c223c0a9d5c27d562a8a6a62d376
gokulkumarrajagopalan <98198356+gokulkumarrajagopalan@users.noreply.github.com>
2024-08-12 21:48:46 +0530

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -2,12 +2,11 @@ const API_URLS = {
   // development: "http://localhost:3700",
   // production : "http://localhost:3700"
 
-
   production: "https://server.gdest.in",
+
 
   // production: "https://3700-idx-project-1720162691714.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev"
 
-  // production: "https://6m894k-3700.csb.app",
 };
 
 export default API_URLS;
