db952a5b8686c51b4940f30aaf1d148913e6d6a0
Gokul <98198356+gokulkumarrajagopalan@users.noreply.github.com>
2024-12-06 14:19:43 +0000

    commit

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,17 +1,7 @@
 const API_URLS = {
-
-  production: "https://server.gdest.in",
-  // production: "https://ty376c-3710.csb.app",
-// production: "https://3700-idx-project-1720162691714.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev",
+ production: "https://server.gdest.in",
+ // production: "https://3700-idx-project-1720162691714.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev",
+ // production: "https://ty376c-3700.csb.app",
 };
 
-const API_UI_URLS = {
-production:  "https://gdest.in",
-// production: "https://ty376c-3000.csb.app",
-// production: "https://3000-idx-project-1720162691714.cluster-3g4scxt2njdd6uovkqyfcabgo6.cloudworkstations.dev",
-
-};
-
-export { API_URLS, API_UI_URLS };
-
- 
+export default API_URLS;
